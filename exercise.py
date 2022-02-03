9db68084fec3f779a4c4df2b50c76974fd447266
gregmrnt <gregmarnet@hotmail.fr>
2022-02-03 19:52:07 +0100

    test

--- exercise.py
+++ exercise.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 # Logic
 
@@ -9,7 +10,11 @@ def check_uniqueness(lst):
     Check if a list contains only unique values.
     Returns True only if all values in the list are unique, False otherwise
     """
-    pass
+
+    # We first extract the unique values of lst
+    length = np.unique(np.array(lst))
+    
+    return (length == len(lst))
 
 
 def smallest_difference(array):
@@ -19,7 +24,19 @@ def smallest_difference(array):
     Please note that the array can be large and that the more
     computationally efficient the better
     """
-    pass
+    
+    array = np.sort(array)
+    diffMin=array.max()
+    
+    for i in range(len(array)-1):
+        
+        diff = abs(array[i]-array[i+1])
+        
+        if diff < diffMin :
+            
+            diffMin = diff
+            
+    return diffMin
 
 
 # Finance and DataFrame manipulation
@@ -33,7 +50,16 @@ def macd(prices, window_short=12, window_long=26):
     Assume simple moving average rather than exponential moving average
     The expected output is in the output.csv file
     """
-    pass
+    
+    # WE assume that the prices column is always at index 1 on the DataFrame
+    
+    mean_short_window = prices.iloc[:,1].rolling(window_short).mean()
+    mean_long_window = prices.iloc[:,1].rolling(window_long).mean()
+    
+    prices["macd_12_26"] = mean_short_window - mean_long_window
+    
+    
+    return prices
 
 
 def sortino_ratio(prices):
@@ -43,7 +69,23 @@ def sortino_ratio(prices):
     Assume risk-free rate = 0
     On the given test set, it should yield 0.05457
     """
-    pass
+    
+    # We'd simply add a 'for' loop on prices.iloc[:,i] if we have several stocks to treat 
+    
+    returns = prices.iloc[:,1].pct_change()[1:]
+    
+    returns_neg = returns[returns <= 0]
+    
+    downside_vol = returns_neg.std()
+    
+    mean = returns.mean()
+    
+    rf = 0
+    
+    sortino_ratio = (mean  - rf) / downside_vol
+    
+    
+    return sortino_ratio
 
 
 def expected_shortfall(prices, level=0.95):
@@ -52,7 +94,15 @@ def expected_shortfall(prices, level=0.95):
     returns the expected shortfall at a given level
     On the given test set, it should yield -0.03468
     """
-    pass
+    # We skip the first value that should be NAN
+    
+    returns = prices.iloc[:,1].pct_change()[1:]
+    
+    VAR = np.quantile(returns,1-level)
+    
+    shortfall = returns[returns <= VAR].mean()
+    
+    return shortfall
 
 
 # Plot
@@ -63,4 +113,16 @@ def visualize(prices, path):
     Code a function that takes a DataFrame named prices and
     saves the plot to the given path
     """
+    name = prices.iloc[:,1].name
+    
+    plt.plot(prices.iloc[:,1])
+    
+    plt.title(f"{name} stock price")
+    plt.savefig(path + "/" + name + ".png")
+    
     pass
+
+
+
+
+
